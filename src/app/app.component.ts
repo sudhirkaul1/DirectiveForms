8b0455cbdf2de51565672a6ec8312932c1b582ad
Sudhir Kaul <sudhirkaul1@gmail.com>
2022-08-04 13:29:35 +1200

    Angular Directive Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+  @ViewChild('f') submitForm: NgForm;
+  answer: string;
+  defaultQuestion = 'pet';
+
+  genders = ['male','female'];
+
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  }
+
+  submitted = false;
+
+  suggestUserName(){
+    const suggestedName = 'Superuser';
+    // this.submitForm.setValue(
+    //   {
+    //     username: suggestedName,
+    //     email: ''
+    //   }
+    // );
+    this.submitForm.form.patchValue(
+      {
+        username: suggestedName
+      }
+    );
+  }
+
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+  // }
+
+  onSubmit(){
+    this.submitted = true;
+    this.user.username = this.submitForm.value.username;
+    this.user.email = this.submitForm.value.email;
+    this.user.gender = this.submitForm.value.gender;
+    this.user.secret = this.submitForm.value.secret;
+    this.user.answer = this.submitForm.value.questionAnswer;
+
+    this.submitForm.reset();
+  }
+
 }
